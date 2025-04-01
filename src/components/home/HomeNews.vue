c75b5e54b596e157fbcac886a09ef9e956308ea8
Bander Btati <bander.mohammed.2000@gmail.com>
2025-04-01 23:42:20 +0300

    part of news section done

--- src/components/home/HomeNews.vue
+++ src/components/home/HomeNews.vue
@@ -2,10 +2,18 @@
   <section id="news">
     <div class="container-fluid py-5">
       <div class="row">
-        <div class="col-md-4"></div>
+        <div class="col-md-4 order-1 order-md-0 mt-5 mt-md-0">
+          <div class="col">
+            <h5 class="mb-4">
+              Hall Of <span class="light-pink-color">Fame</span>
+            </h5>
+          </div>
+          <list-people :people="reviewers">Top Reviewers</list-people>
+          <list-people :people="fandom">Top Fandom</list-people>
+        </div>
         <div class="col-md-8">
           <div class="row">
-            <div class="col-6"><h5 class="mb-4 title-section">News</h5></div>
+            <div class="col-6"><h5 class="mb-4">News</h5></div>
             <div class="col-6 text-end">
               <span class="prev me-3" @click="movePrev">
                 <i class="fa-solid fa-chevron-left fa-xl"></i>
@@ -50,30 +58,6 @@
                   />
                 </div>
               </div>
-              <!-- <button
-                class="carousel-control-prev"
-                type="button"
-                data-bs-target="#carouselExample"
-                data-bs-slide="prev"
-              >
-                <span
-                  class="carousel-control-prev-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Previous</span>
-              </button>
-              <button
-                class="carousel-control-next"
-                type="button"
-                data-bs-target="#carouselExample"
-                data-bs-slide="next"
-              >
-                <span
-                  class="carousel-control-next-icon"
-                  aria-hidden="true"
-                ></span>
-                <span class="visually-hidden">Next</span>
-              </button> -->
             </div>
           </div>
         </div>
@@ -84,15 +68,31 @@
 
 <script>
 import NewsCard from "./NewsCard.vue";
+import ListPeople from "./ListPeople.vue";
 import { Carousel } from "bootstrap";
 
 export default {
   components: {
     NewsCard,
+    ListPeople,
   },
   data() {
     return {
       carousel: null,
+      reviewers: [
+        { name: "Bader Ali", img: "/people/1.jpg", points: "2544" },
+        { name: "Nader Fahed", img: "/people/6.jpg", points: "1589" },
+        { name: "Ali Ahmed", img: "/people/3.png", points: "4" },
+        { name: "Bander Mohammed", img: "/people/4.png", points: "188" },
+        { name: "Faisal Salem", img: "/people/5.png", points: "2223" },
+      ],
+      fandom: [
+        { name: "Bader Ali", img: "/people/1.jpg", points: "2544" },
+        { name: "Nader Fahed", img: "/people/6.jpg", points: "1589" },
+        { name: "Ali Ahmed", img: "/people/3.png", points: "4" },
+        { name: "Bander Mohammed", img: "/people/4.png", points: "188" },
+        { name: "Faisal Salem", img: "/people/5.png", points: "2223" },
+      ],
     };
   },
   mounted() {
@@ -122,9 +122,13 @@ section#news {
   background-color: #0f152b;
 }
 .news-section {
+  /* padding: 43px 30px; */
   padding: 30px;
   background-color: #0c0f22;
   border-radius: 5px;
+}
+h5 {
+  color: #ffffff;
 }
 .title-section {
   color: #ffffff;
---
+++ src/components/home/ListPeople.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="col">
+    <h6>
+      <slot></slot>
+    </h6>
+  </div>
+  <ul class="mt-4">
+    <li v-for="(person, index) in people" :key="index">
+      <div class="info">
+        <img :src="person.img" alt="person image" loading="lazy" />
+        <p class="mb-0" :title="person.name">{{ person.name }}</p>
+      </div>
+      <span class="points">{{ person.points }}</span>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["people"],
+};
+</script>
+
+<style scoped>
+h6 {
+  position: relative;
+  color: #e8e8e8;
+  font-size: 16px;
+  font-weight: normal;
+  padding-bottom: 19px;
+  /* border-bottom: 1px solid #686868; */
+  border-bottom: 1px solid #353535;
+}
+h6::after {
+  content: "";
+  position: absolute;
+  bottom: -1px;
+  left: 0;
+  width: 120px;
+  height: 1px;
+  background-color: #e2606a;
+}
+ul {
+  list-style: none;
+  padding: 0;
+}
+ul li {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 12px;
+}
+ul li .info {
+  display: flex;
+  align-items: center;
+  margin-right: 15px;
+  /* -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical; */
+  /* overflow: hidden; */
+}
+
+.info img {
+  width: 42px;
+  height: 42px;
+  border-radius: 50%;
+}
+.info p {
+  font-size: 15px;
+  margin-left: 12px;
+  color: #e8e8e8;
+  display: -webkit-box;
+  -webkit-line-clamp: 1;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  word-break: break-all; /* يسمح بالكسر عند أي حرف */
+  overflow-wrap: anywhere;
+}
+.points {
+  font-size: 14px;
+  color: #19947b;
+  border: 1px solid #19947b;
+  padding: 1px 10px;
+  border-radius: 50px;
+}
+</style>
